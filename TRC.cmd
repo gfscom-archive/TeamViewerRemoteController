cd2a12c987d10ef436473b57f3a1793d38529f20
Giovanni Francesco Solone <dev@gfsolone.com>
2018-05-14 12:15:08 +0200

    Update 201805141215

--- TRC.cmd
+++ TRC.cmd
@@ -1,17 +1,18 @@
 ::	TeamViewer Remote Controller
 ::	-----------------------------------------------------------------------------------------------------------
 ::	Autore:				GSolone
-::	Versione:			0.3
+::	Versione:			0.4
 ::	Utilizzo:			.\TRC.cmd NOMEMACCHINA
 ::						ATTENZIONE: va utilizzato come amministratore locale o 
 ::						di dominio della macchina alla quale si punta.
 ::	Info:				https://gioxx.org/tag/teamviewer
-::	Ultima modifica:	10-01-2018
+::	Ultima modifica:	07-02-2018
 ::	Fonti utilizzate:	https://gist.github.com/mlocati/fdabcaeb8071d5c75a2d51712db24011#file-win10colors-cmd
 ::						http://www.robvanderwoude.com/parameters.php
 ::						https://stackoverflow.com/questions/4419868/what-is-the-current-directory-in-a-batch-file
 ::	------------------------------------------------------------------------------------------------------------
-::	== Modifiche ==
+::	Modifiche:
+::		0.4- evito la ripartenza del ciclo StartEngine al termine di una modifica via prompt / TRC.
 ::		0.3- ho isolato lo script e tutti i VBS, per poterlo "trasportare facilmente altrove". Ho integrato la nuova funzione di modifica Permanent Password da remoto. Ho tolto anche i riferimenti assoluti al programma.
 ::		0.2- modificate spaziature in output VBS, modificato colore dell'output.
 
@@ -56,26 +57,26 @@ goto END
 echo [92m
 cscript /nologo %~dp0bin\TeamViewerRemoteKill.vbs %1
 echo [0m
-goto StartEngine
+goto END
 
 :RemotePassword
 echo [92m
 cscript /nologo %~dp0bin\TeamViewerRemotePasswd.vbs %1
 echo [0m
-goto StartEngine
+goto END
 
 :RemoteHost
 echo [92m
 cscript /nologo %~dp0bin\TeamViewerRemoteHost.vbs %1
 echo [0m
-goto StartEngine
+goto END
 
 :HostPassword
 echo [92m
 cscript /nologo %~dp0bin\TeamViewerRemotePasswd.vbs %1
 cscript /nologo %~dp0bin\TeamViewerRemoteHost.vbs %1
 echo [0m
-goto StartEngine
+goto END
 
 ::	--  Messaggi di programma  ---------------------------------------------------------------------------------
 
